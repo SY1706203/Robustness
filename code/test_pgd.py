8a292659eb591fc765a0a6d2d76f6b8c106ae30d
ZongyueLi <zongyue.li@outlook.com>
2021-08-07 01:17:23 +0200

    FIX[GROC]: previous commit with 2 trained models with 2 different perturbations are wrong. To do GROC, only 2 different adjacency matrix are needed and get 2 different Embeddings from the original model. Then using these 2 Embeddings, GROC training is done, and eval/test are done based on original model.

--- code/test_pgd.py
+++ code/test_pgd.py
@@ -39,6 +39,9 @@ torch.manual_seed(args.seed)
 if device != 'cpu':
     torch.cuda.manual_seed(args.seed)
 
+# TODO: move this param to parser
+train_groc = True
+
 adj = dataset.getSparseGraph()
 adj = torch.FloatTensor(adj.todense()).to(device)
 
@@ -57,12 +60,14 @@ Recmodel = Recmodel.to(device)
 num_users = Recmodel.num_users
 # adj=adj.to(device)
 Recmodel.fit(adj, users, posItems, negItems)
-
+print("original model performance:")
+print("===========================")
 Procedure.Test(dataset, Recmodel, 100, utils.normalize_adj_tensor(adj), None, 0)
+print("===========================")
 # Setup Attack Model
 
 
-def attack_model(recmodel, adj_matrix, perturbations):
+def attack_model(recmodel, adj_matrix, perturbations, train_groc_):
     model = PGDAttack(model=recmodel, nnodes=adj_matrix.shape[0], loss_type='CE', device=device)
 
     model = model.to(device)
@@ -75,13 +80,15 @@ def attack_model(recmodel, adj_matrix, perturbations):
     Recmodel_ = lightgcn.LightGCN(device)
     Recmodel_ = Recmodel_.to(device)
     # adj=adj.to(device)
-    Recmodel_.fit(modified_adj, users, posItems, negItems)
+    print("train the model with modified adjacency matrix")
+    if not train_groc_:
+        Recmodel_.fit(modified_adj, users, posItems, negItems)
 
-    # Procedure.Test(dataset, Recmodel_, 1, utils.normalize_adj_tensor(modified_adj), None, 0)
-    return modified_adj, Recmodel_
+        Procedure.Test(dataset, Recmodel_, 1, utils.normalize_adj_tensor(modified_adj), None, 0)
+    return modified_adj
 
 
-def pre_dcl_loss(recmodel, modified_adj, users_, poss):
+def get_embed_groc(recmodel, modified_adj, users_, poss):
     (users_emb, pos_emb, _, _) = recmodel.getEmbedding(modified_adj, users_.long(), poss.long())
     # pos_emb_old=pos_emb
     users_emb = nn.functional.normalize(users_emb, dim=1)
@@ -96,18 +103,19 @@ def pre_dcl_loss(recmodel, modified_adj, users_, poss):
     return torch.cat([users_emb, pos_emb])
 
 
-def dcl_loss_vec(recmodel_a, recmodel_b, modified_adj_a, modified_adj_b, users_, poss):
-    all_emb_a = pre_dcl_loss(recmodel_a, modified_adj_a, users_, poss)
-    all_emb_b = pre_dcl_loss(recmodel_b, modified_adj_b, users_, poss)
+def groc_loss_vec(recmodel, modified_adj_a, modified_adj_b, users_, poss):
+    all_emb_a = get_embed_groc(recmodel, modified_adj_a, users_, poss)
+    all_emb_b = get_embed_groc(recmodel, modified_adj_b, users_, poss)
 
-    contrastive_similarity = torch.exp(torch.diag(torch.matmul(all_emb_a, all_emb_b.t().contiguous())) / recmodel_a.T)
+    contrastive_similarity = torch.exp(torch.diag(torch.matmul(all_emb_a, all_emb_b.t().contiguous())) / recmodel.T)
     # contrastive_similarity size： [batch_size,]
     self_neg_similarity_matrix = torch.matmul(all_emb_a, all_emb_a.t().contiguous())  # z1 * z1
     contrastive_neg_similarity_matrix = torch.matmul(all_emb_a, all_emb_b.t().contiguous())  # z1 * z2
     # self_neg_contrastive_similarity_matrix size： [batch_size, batch_size]
     
     # mask diagonal
-    mask = torch.eye(all_emb_b.size(0), all_emb_b.size(0)).bool()  # mask with diagonal all True others all False
+    mask = torch.eye(all_emb_b.size(0), all_emb_b.size(0)).bool().to(device)
+    # tensor mask with diagonal all True others all False
     self_neg_similarity_matrix.masked_fill_(mask, 0)
     contrastive_neg_similarity_matrix.masked_fill_(mask, 0)
     # concatenates z1 * z1 with 0-diagonal and z1 * z2 with 0-diagonal in row
@@ -115,43 +123,38 @@ def dcl_loss_vec(recmodel_a, recmodel_b, modified_adj_a, modified_adj_b, users_,
     neg_contrastive_similarity_matrix = \
         torch.cat([self_neg_similarity_matrix, contrastive_neg_similarity_matrix], -1)
     # sum the matrix up by row
-    neg_contrastive_similarity = torch.sum(torch.exp(neg_contrastive_similarity_matrix) / recmodel_a.T, 1)
+    neg_contrastive_similarity = torch.sum(torch.exp(neg_contrastive_similarity_matrix) / recmodel.T, 1)
 
     loss_vec = -torch.log(contrastive_similarity / contrastive_similarity + neg_contrastive_similarity)
 
     return loss_vec
 
 
-def dcl_loss(recmodel_a, recmodel_b, modified_adj_a, modified_adj_b, users_, poss):
-    loss_vec_a = dcl_loss_vec(recmodel_a, recmodel_b, modified_adj_a, modified_adj_b, users_, poss)
-    loss_vec_b = dcl_loss_vec(recmodel_a, recmodel_b, modified_adj_b, modified_adj_a, users_, poss)
+def groc_loss(ori_model, modified_adj_a, modified_adj_b, users_, poss):
+    loss_vec_a = groc_loss_vec(ori_model, modified_adj_a, modified_adj_b, users_, poss)
+    loss_vec_b = groc_loss_vec(ori_model, modified_adj_b, modified_adj_a, users_, poss)
 
     return torch.sum(torch.add(loss_vec_a, loss_vec_b)) / (2 * loss_vec_a.size(0))
 
 
-def dcl_train():
-    modified_adj_a, Rec_model_a = attack_model(Recmodel, adj, perturbations_a)
-    modified_adj_b, Rec_model_b = attack_model(Recmodel, adj, perturbations_b)
+def groc_train(train_groc_, ori_model):
+    modified_adj_a = attack_model(Recmodel, adj, perturbations_a, train_groc_)
+    modified_adj_b = attack_model(Recmodel, adj, perturbations_b, train_groc_)
 
-    Rec_model_a.train()
-    Rec_model_b.train()
-    optimizer_grouped_parameters = [
-        {
-            "params": [p for n, p in Rec_model_a.named_parameters()] + [p for n, p in Rec_model_b.named_parameters()]
-        }
-        ]
-    optimizer = optim.Adam(optimizer_grouped_parameters, lr=Rec_model_a.lr, weight_decay=Rec_model_a.weight_decay)
+    ori_model.train()
+
+    optimizer = optim.Adam(ori_model.parameters(), lr=ori_model.lr, weight_decay=ori_model.weight_decay)
     for i in range(100):
         optimizer.zero_grad()
-        users_ = users.to(Rec_model_a.device)
-        posItems_ = posItems.to(Rec_model_a.device)
-        negItems_ = negItems.to(Rec_model_a.device)
+        users_ = users.to(ori_model.device)
+        posItems_ = posItems.to(ori_model.device)
+        negItems_ = negItems.to(ori_model.device)
         users_, posItems_, negItems_ = utils.shuffle(users_, posItems_, negItems_)
         total_batch = len(users) // 2048 + 1
         aver_loss = 0.
         for (batch_i, (batch_users, batch_pos, _)) \
                 in enumerate(utils.minibatch(users_, posItems_, negItems_, batch_size=2048)):
-            loss = dcl_loss(Rec_model_a, Rec_model_b, modified_adj_a, modified_adj_b, batch_users, batch_pos)
+            loss = groc_loss(ori_model, modified_adj_a, modified_adj_b, batch_users, batch_pos)
 
             loss.backward()
             optimizer.step()
@@ -159,8 +162,18 @@ def dcl_train():
             aver_loss += loss.cpu().item()
         aver_loss = aver_loss / total_batch
         if i % 10 == 0:
-            print("DCL Loss: ", aver_loss)
+            print("GROC Loss: ", aver_loss)
 
 
 # TODO: optimize the code in right file!
-dcl_train()
+
+if train_groc:
+    groc_train(train_groc, Recmodel)
+    print("original model performance after GROC learning:")
+    print("===========================")
+    Procedure.Test(dataset, Recmodel, 100, utils.normalize_adj_tensor(adj), None, 0)
+    print("===========================")
+else:
+    _ = attack_model(Recmodel, adj, perturbations_a, train_groc)
+
+
