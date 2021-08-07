FIX[GROC]: make sure the MLP is unique and apply it before normalization

code/test_pgd.py
@@ -92,24 +92,24 @@ def attack_model(recmodel, adj_matrix, perturbations, train_groc_):
     return modified_adj
 
 
-def get_embed_groc(recmodel, modified_adj, users_, poss):
+def get_embed_groc(recmodel, modified_adj, users_, poss, mlp):
     (users_emb, pos_emb, _, _) = recmodel.getEmbedding(modified_adj, users_.long(), poss.long())
     # pos_emb_old=pos_emb
-    users_emb = nn.functional.normalize(users_emb, dim=1)
-    pos_emb = nn.functional.normalize(pos_emb, dim=1)
-
-    mlp = utils.MLP(users_emb.size(-1), users_emb.size(-1))
-    mlp = mlp.to(device)
 
     users_emb = mlp(users_emb)
     pos_emb = mlp(pos_emb)
+
+    users_emb = nn.functional.normalize(users_emb, dim=1)
+    pos_emb = nn.functional.normalize(pos_emb, dim=1)
 
     return torch.cat([users_emb, pos_emb])
 
 
 def groc_loss_vec(recmodel, modified_adj_a, modified_adj_b, users_, poss):
-    all_emb_a = get_embed_groc(recmodel, modified_adj_a, users_, poss)
-    all_emb_b = get_embed_groc(recmodel, modified_adj_b, users_, poss)
+    mlp = utils.MLP(64, 64)
+    mlp = mlp.to(device)
+    all_emb_a = get_embed_groc(recmodel, modified_adj_a, users_, poss, mlp)
+    all_emb_b = get_embed_groc(recmodel, modified_adj_b, users_, poss, mlp)
 
     contrastive_similarity = torch.exp(torch.diag(torch.matmul(all_emb_a, all_emb_b.t().contiguous())) / recmodel.T)
     # contrastive_similarity size： [batch_size,]