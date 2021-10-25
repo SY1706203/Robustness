DEV[traing]: use groc_with_bpr_cat in right place and fix bug in PGD attack. In PGD attack, while calculating the gradient over adj_changes, the tensor production inside GCN should be dense * dense, if use sparse adj, will return error.

code/groc_loss.py
@@ -355,7 +355,7 @@ class GROC_loss(nn.Module):
                 print("BPR Loss: ", aver_bpr_loss)
                 print("DCL Loss: ", aver_dcl_loss)
 
-    def groc_train_with_bpr(self, data_len_, users, posItems, negItems):
+    def groc_train_with_bpr(self, data_len_, users, posItems, negItems, perturbations):
         self.ori_model.train()
         embedding_param = []
         adj_param = []
@@ -396,21 +396,21 @@ class GROC_loss(nn.Module):
             aver_groc_loss = 0.
             for (batch_i, (batch_users, batch_pos, batch_neg)) \
                     in enumerate(utils.minibatch(users, posItems, negItems, batch_size=self.args.batch_size)):
-                batch_items = utils.shuffle(torch.cat((batch_pos, batch_neg))).to(self.device)
+                # batch_items = utils.shuffle(torch.cat((batch_pos, batch_neg))).to(self.device)
 
                 batch_users_unique = batch_users.unique()  # only select 10 anchor nodes for adj_edge insertion
+                mask_1 = (torch.FloatTensor(self.ori_model.latent_dim).uniform_() < self.args.mask_prob_1) \
+                    .to(self.device)
+                mask_2 = (torch.FloatTensor(self.ori_model.latent_dim).uniform_() < self.args.mask_prob_2) \
+                    .to(self.device)
                 if self.args.use_groc_framework:
                     adj_with_insert = self.get_modified_adj_for_insert(batch_users_unique, adj_with_2_hops)  # 2 views are same
-
-                    mask_1 = (torch.FloatTensor(self.ori_model.latent_dim).uniform_() < self.args.mask_prob_1) \
-                        .to(self.device)
-                    mask_2 = (torch.FloatTensor(self.ori_model.latent_dim).uniform_() < self.args.mask_prob_2) \
-                        .to(self.device)
 
                     # batch_users_groc = batch_all_node[batch_all_node < self.num_users]
                     # batch_items = batch_all_node[batch_all_node >= self.num_users] - self.num_users
 
-                    adj_for_loss_gradient = utils.normalize_adj_tensor(adj_with_insert.to_sparse(), self.d_mtr, sparse=True)
+                    adj_for_loss_gradient = utils.normalize_adj_tensor(adj_with_insert.to_sparse(), self.d_mtr,
+                                                                       sparse=True)
 
                     if not self.args.use_IntegratedGradient:
                         loss_for_grad = ori_gcl_computing(self.ori_adj, self.ori_model, adj_for_loss_gradient,
@@ -448,11 +448,23 @@ class GROC_loss(nn.Module):
                     adj_norm_1 = utils.normalize_adj_tensor(adj_insert_remove_1.to_sparse(), self.d_mtr, sparse=True)
                     adj_norm_2 = utils.normalize_adj_tensor(adj_insert_remove_2.to_sparse(), self.d_mtr, sparse=True)
 
+                    del adj_insert_remove_1
+                    del adj_insert_remove_2
+
+                else:
+                    adj_pgd_1 = self.pgd_model.attack_per_batch(self.ori_adj, perturbations, batch_users,
+                                                                batch_pos, batch_neg, self.num_users)
+                    adj_pgd_2 = self.pgd_model.attack_per_batch(self.ori_adj, perturbations, batch_users,
+                                                                batch_pos, batch_neg, self.num_users)
+
+                    adj_norm_1 = utils.normalize_adj_tensor(adj_pgd_1.to_sparse(), self.d_mtr, sparse=True)
+                    adj_norm_2 = utils.normalize_adj_tensor(adj_pgd_2.to_sparse(), self.d_mtr, sparse=True)
+
+                    gc.collect()
+
                 groc_loss = ori_gcl_computing(self.ori_adj, self.ori_model, adj_norm_1, adj_norm_2, batch_users,
                                               batch_pos, self.args, self.device, mask_1=mask_1, mask_2=mask_2)
 
-                del adj_insert_remove_1
-                del adj_insert_remove_2
                 del adj_norm_1
                 del adj_norm_2
 
@@ -612,8 +624,7 @@ class GROC_loss(nn.Module):
                 # batch_users_groc = batch_all_node[batch_all_node < self.num_users]
                 # batch_items = batch_all_node[batch_all_node >= self.num_users] - self.num_users
 
-                adj_for_loss_gradient = utils.normalize_adj_tensor(adj_with_insert.to_sparse(), self.d_mtr,
-                                                                   sparse=True)
+                adj_for_loss_gradient = utils.normalize_adj_tensor(adj_with_insert.to_sparse(), self.d_mtr, sparse=True)
 
                 if self.args.normal_gradients:
                     gcl_grad = ori_gcl_computing(self.ori_adj, self.ori_model, adj_for_loss_gradient,

code/lightgcn.py
@@ -6,7 +6,7 @@ import utils
 
 
 class LightGCN(nn.Module):
-    def __init__(self, device=None):
+    def __init__(self, device=None, sparse=True):
         super(LightGCN, self).__init__()
 
         assert device is not None, "Please specify 'device'!"
@@ -18,6 +18,7 @@ class LightGCN(nn.Module):
         self.num_items = dataset.m_item
         self.latent_dim = 64
         self.f = nn.Sigmoid()
+        self._is_sparse = sparse
         self.adj = None
 
         self.tau_plus = 1e-3
@@ -42,13 +43,21 @@ class LightGCN(nn.Module):
         pass
 
     def fit(self, adj, users, posItems, negItems):
-        if type(adj) is not torch.Tensor:
-            adj_norm = utils.normalize_adj_tensor(adj, sparse=True)
-            adj = utils.to_tensor(adj_norm, device=self.device)
+        if self._is_sparse:
+            if type(adj) is not torch.Tensor:
+                adj_norm = utils.normalize_adj_tensor(adj, sparse=True)
+                adj = utils.to_tensor(adj_norm, device=self.device)
+            else:
+                adj_norm = utils.normalize_adj_tensor(adj, sparse=True)
+                adj = adj_norm.to(self.device)
+            # self.adj=adj
         else:
-            adj_norm = utils.normalize_adj_tensor(adj, sparse=True)
-            adj = adj_norm.to(self.device)
-        # self.adj=adj
+            if type(adj) is not torch.Tensor:
+                adj_norm = utils.normalize_adj_tensor(adj)
+                adj = utils.to_tensor(adj_norm, device=self.device)
+            else:
+                adj_norm = utils.normalize_adj_tensor(adj)
+                adj = adj_norm.to(self.device)
 
         self._train_without_val(adj, users, posItems, negItems)
 
@@ -77,7 +86,10 @@ class LightGCN(nn.Module):
         g_droped = adj
 
         for layer in range(self.n_layers):
-            all_emb = torch.sparse.mm(g_droped, all_emb)
+            if self._is_sparse:
+                all_emb = torch.sparse.mm(g_droped, all_emb)
+            else:
+                all_emb = torch.mm(g_droped, all_emb)
             embs.append(all_emb)
         embs = torch.stack(embs, dim=1)
         # print(embs.size())

code/test_pgd.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import lightgcn
 from register import dataset
+from topology_attack import PGDAttack
 import utils
 from utils_attack import attack_model, attack_randomly, attack_embedding
 import Procedure
@@ -178,8 +179,9 @@ if args.train_groc:
 
     if args.groc_with_bpr:
         print("Mode:GROC + BPR")
-        groc = GROC_loss(Recmodel, adj, d_mtr, args)
-        groc.groc_train_with_bpr(data_len, users, posItems, negItems)
+        pgd_model = PGDAttack(model=Recmodel, nnodes=adj.size(1), device=device)
+        groc = GROC_loss(Recmodel, adj, d_mtr, args, pgd_model)
+        groc.groc_train_with_bpr(data_len, users, posItems, negItems, perturbations)
 
         print("save model")
         torch.save(Recmodel.state_dict(), os.path.abspath(os.path.dirname(os.getcwd())) +
@@ -237,7 +239,7 @@ if args.train_groc:
     if args.groc_with_bpr_cat:
         print("Mode:GROC + BPR CAT")
         groc = GROC_loss(Recmodel, adj, d_mtr, args)
-        groc.groc_train_with_bpr(data_len, users, posItems, negItems)
+        groc.groc_train_with_bpr_cat(data_len, users, posItems, negItems)
 
         print("save model")
         torch.save(Recmodel.state_dict(), os.path.abspath(os.path.dirname(os.getcwd())) +

code/topology_attack.py
@@ -1,7 +1,6 @@
 import torch
 import numpy as np
 from tqdm import tqdm
-import scipy.sparse as sp
 from torch.nn.parameter import Parameter
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,17 +8,14 @@ from torch import optim
 from base_attack import BaseAttack
 import utils
 
-from register import dataset
-import world
-
 
 class PGDAttack(BaseAttack):
     def __init__(self, model=None, nnodes=None, loss_type='CE', feature_shape=None, attack_structure=True,
-                 attack_features=False, device='cuda:0'):
+                 attack_features=False, device=None):
         super(PGDAttack, self).__init__(model, nnodes, attack_structure, attack_features, device)
 
         assert attack_features or attack_structure, 'attack_features or attack_structure cannot be both False'
-
+        self.surrogate._is_sparse = False
         self.loss_type = loss_type
         self.modified_adj = None
         self.modified_features = None
@@ -80,6 +76,32 @@ class PGDAttack(BaseAttack):
 
         torch.save(self.modified_adj, path.format(ids[flag]))
 
+    def attack_per_batch(self, ori_adj, perturbations, batch_users, batch_pos, batch_neg, num_users):
+
+        victim_model = self.surrogate
+        victim_model.eval()
+
+        modified_adj = self.get_modified_adj(ori_adj, num_users)
+        adj_norm = utils.normalize_adj_tensor(modified_adj)
+
+        loss, reg_loss = victim_model.bpr_loss(adj_norm, batch_users, batch_pos, batch_neg)
+
+        adj_grad = torch.autograd.grad(loss, self.adj_changes, retain_graph=True)[0]
+
+        # lr=200/np.sqrt(t+1)
+        lr = 200
+        self.adj_changes.data.copy_(lr * adj_grad)
+        # print(self.adj_changes) used in perturbation 0.1 log
+        self.projection(perturbations)
+
+        self.random_sample_batch(victim_model, ori_adj, perturbations, batch_users, batch_pos, batch_neg, num_users)
+
+        adj_modified = self.get_modified_adj(ori_adj, num_users).detach()
+
+        self.adj_changes.data.fill_(0)
+
+        return adj_modified
+
     def random_sample(self, ori_adj, perturbations, users, posItems, negItems, num_users):
         K = 5
         best_loss = -1000
@@ -121,6 +143,34 @@ class PGDAttack(BaseAttack):
                     best_s = sampled
             self.adj_changes.data.copy_(torch.tensor(best_s))
 
+    def random_sample_batch(self, victim_model, ori_adj, perturbations, batch_users, batch_pos, batch_neg, num_users):
+        K = 5
+        best_loss = -1000
+        with torch.no_grad():
+            s = self.adj_changes
+            for i in range(K):
+                sampled = torch.distributions.binomial.Binomial(1, s).sample()
+
+                # print(sampled.sum())
+                if sampled.sum() > perturbations:
+                    continue
+                self.adj_changes.data.copy_(sampled)
+
+                modified_adj = self.get_modified_adj(ori_adj, num_users)
+                adj_norm = utils.normalize_adj_tensor(modified_adj)
+
+                loss_total = 0.
+
+                loss, reg_loss = victim_model.bpr_loss(adj_norm, batch_users, batch_pos, batch_neg)
+
+                # print(loss)
+                loss_total += loss.cpu().item()
+
+                if best_loss < loss_total:
+                    best_loss = loss_total
+                    best_s = sampled
+            self.adj_changes.data.copy_(best_s)
+
     def _loss(self, output, labels):
         if self.loss_type == "CE":
             loss = F.nll_loss(output, labels)
@@ -154,11 +204,6 @@ class PGDAttack(BaseAttack):
         # modified_adj=m+ori_adj
         modified_adj[:num_users, :num_users] = 0
         modified_adj[num_users:, num_users:] = 0
-
-        # set diagonal to 0
-        adj_diag = torch.diag(modified_adj)
-        adj_diag_matrix = torch.diag_embed(adj_diag)
-        modified_adj = modified_adj - adj_diag_matrix
 
         return modified_adj
 